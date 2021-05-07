99f8728243bd140e5b7863ccc15c0814b635dba9
Daniel <dunapanta2@gmail.com>
2021-05-07 14:28:49 -0500

    Adding Back Button

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -9,16 +9,17 @@ import {
   Dimensions,
   ActivityIndicator,
 } from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import {RootStackParams} from '../navigation/Navigation';
 import {useMovieDetails} from '../hooks/useMovieDetails';
 import {MovieDetails} from '../components/MovieDetails';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
 
 const {height} = Dimensions.get('screen');
 
-export const DetailScreen = ({route}: Props) => {
+export const DetailScreen = ({route, navigation}: Props) => {
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
@@ -26,6 +27,16 @@ export const DetailScreen = ({route}: Props) => {
 
   return (
     <ScrollView>
+      <View style={styles.backButton}>
+        <TouchableOpacity onPress={() => navigation.pop()}>
+          <Icon
+            color="white"
+            name="arrow-back-outline"
+            size={50}
+            style={styles.backArrow}
+          />
+        </TouchableOpacity>
+      </View>
       <View style={styles.imageContainer}>
         <Image source={{uri}} style={styles.posterImage} />
       </View>
@@ -48,6 +59,25 @@ export const DetailScreen = ({route}: Props) => {
 const styles = StyleSheet.create({
   posterImage: {
     flex: 1,
+  },
+  backButton: {
+    position: 'absolute',
+    zIndex: 7,
+    elevation: 10,
+    height: 65,
+    width: 65,
+    backgroundColor: 'grey',
+    opacity: 0.7,
+    borderRadius: 50,
+    top: 20,
+    left: 12,
+  },
+  backArrow: {
+    position: 'relative',
+    zIndex: 2,
+    elevation: 3,
+    top: 7,
+    left: 10,
   },
   imageContainer: {
     width: '100%',
