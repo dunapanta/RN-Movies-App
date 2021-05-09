ef6b4f3dd8f95e7060dad88e323bc725649bbe0c
Daniel <dunapanta2@gmail.com>
2021-05-08 20:05:40 -0500

    Extracting colors of movie poster

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import {GradientBackground} from '../components/GradientBackground';
 import HorizontalSlider from '../components/HorizontalSlider';
+import ImageColors from 'react-native-image-colors';
 
 import {PosterMovie} from '../components/PosterMovie';
 import {useMovies} from '../hooks/useMovies';
@@ -25,6 +26,13 @@ export const HomeScreen = () => {
     );
   }
 
+  const getPosterColors = async (index: number) => {
+    const movie = nowPlaying[index];
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const colors = await ImageColors.getColors(uri, {});
+    console.log(colors);
+  };
+
   return (
     <GradientBackground>
       <ScrollView>
@@ -35,6 +43,7 @@ export const HomeScreen = () => {
             itemWidth={width - 75}
             data={nowPlaying}
             inactiveSlideOpacity={0.8}
+            onSnapToItem={index => getPosterColors(index)}
           />
           {/* Peliculas Populares */}
           <HorizontalSlider title="Populares:" movies={popular} />
